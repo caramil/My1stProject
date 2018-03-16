3dfccaeeba527afd0e092c607b40d8f73915f121
caramil <cn_adnan@hotmail.com>
2018-03-16 15:34:18 +0100

    My 1st Commit

---
+++ my_test.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+
+#create chrome instance
+browser = webdriver.Chrome()
+
+#First test
+def test_open_url():
+    url = "http://blazedemo.com/"
+    browser.get(url)
+    assert browser.current_url == url
+
+#Close Chrome instance
+def test_close_browser():
+    browser.close()
+    browser.quit()
